b81d14c5c6bfded3c7fc725e660ee3fd47942681
Mercyrhoda <taiworhodaanuoluwa@gmail.com>
2022-01-07 22:53:09 +0100

    updated menu-items using react hooks useNavigate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import { Routes, Route, } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component'
+
+const HatPage = () => {
+  return (
+    <h3>Hats Page</h3>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      <Routes>
+        <Route exact path='/' element={<HomePage/>}/>
+        <Route  path='/hats' element={<HatPage/>}/>
+      </Routes>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './menu-item.styles.scss'
+import { useNavigate } from 'react-router-dom'
 
-const MenuItem = ({title , imageUrl, size}) => {
+
+const MenuItem = ({title , imageUrl, size, linkUrl}) => {
+  const navigate = useNavigate();
   return (
-    <div className={`${ size } menu-item`}  >
+    <div className={`${ size } menu-item`}  onClick={() =>navigate(`${linkUrl}`)}>
       <div className='background-image' style={{
       backgroundImage: `url(${imageUrl})`
       }} />
